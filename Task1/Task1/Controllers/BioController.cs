d381684360ac692de69917eb5ef463ad2458f936
shahariaryan <shahariarahmed834@gmail.com>
2021-09-15 14:32:21 +0600

    task 1

---
+++ Task1/Task1/Controllers/BioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1.Controllers
+{
+    public class BioController : Controller
+    {
+        public ActionResult BioList()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task1/Task1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+
+    }
+}
