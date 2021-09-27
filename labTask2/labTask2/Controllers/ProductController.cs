09e70e43702398aa2edeb45a2f152d3918182d21
shahariaryan <shahariarahmed834@gmail.com>
2021-09-27 14:01:16 +0600

    lab 2

---
+++ labTask2/labTask2/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using labTask2.Models;
+using System.Data.SqlClient;
+
+namespace labTask2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Products()
+        {
+            return View();
+        }
+        [HttpPost]
+       public ActionResult Products(Product p)
+        {
+            string connString = @"Server=DESKTOP-F2QN1PO\SQLEXPRESS;Database=ZOZO; Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            string query = String.Format("Insert into Products values('{0}','{1}','{2}','{3}')", p.Name, p.Price, p.Quantity, p.Discription);
+            SqlCommand cmd = new SqlCommand(query,conn);
+            conn.Open();
+            int r= cmd.ExecuteNonQuery();
+            conn.Close();
+            return View();
+        }
+    }
+}
---
+++ labTask2/labTask2/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labTask2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public string Quantity { get; set; }
+
+        public string  Discription { get; set; }
+    }
+}
